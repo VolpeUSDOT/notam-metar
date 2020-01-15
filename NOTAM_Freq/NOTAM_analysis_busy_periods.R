e48fa8e51584c6142cc2187e2a769c3940cf641c
lyle-tripp-volpe <59397246+lyle-tripp-volpe@users.noreply.github.com>
2020-01-15 14:43:31 -0500

    adding graphics
    creates individual graphics for the days when NOTAM activity is greatest as measured by the busiest block.  creates a summary graphic illustrating the CDF of NOTAMs on those busy days.

--- NOTAM_Freq/NOTAM_analysis_busy_periods.R
+++ NOTAM_Freq/NOTAM_analysis_busy_periods.R
@@ -1,20 +1,21 @@
 load("NOTAM_Freq.RData")
+library(ggplot2)
 
 min_time_on_call_worker <- 4L
 block_size <- min_time_on_call_worker
-top_block_list_size <- 10
+top_block_list_size <- 50
 
 study_start <- min(UniqueInteractions$datetimes)
 study_length <- as.integer(1 + difftime(max(UniqueInteractions$datetimes), study_start, units = "hours"))
 
 hourly_bin_start <- c(seq(1, study_length))
-hourly_bin_start <- (hourly_bin_start-1)*(60*60) + study_start
+hourly_bin_start <- as.POSIXct((hourly_bin_start-1)*(60*60) + study_start, origin = "1970-01-01", tz = "UTC")
 hourly_bin_end <- hourly_bin_start + 60*60
 
 df = 
   # head(
   data.frame(hourly_bin_start, hourly_bin_end, hourly_count = -1, block_count = -1, block_rank_descending = -1)
-  # , 100)
+  # , 1000)
 
 if (!("hour_number" %in% colnames(UniqueInteractions))){
   hour_number <- as.integer(1 + difftime(UniqueInteractions$datetimes, study_start, units = "hours"))
@@ -49,8 +50,33 @@ for(j in 2:(top_block_list_size)){
       # )
 }
 
-print(orig_df[as.POSIXct(orig_df$hourly_bin_start, origin = "1970-01-01", tz = "GMT") %in% as.vector(confirmed_blocks), ])
+busy_periods <- orig_df[orig_df$hourly_bin_start %in% as.vector(confirmed_blocks), ]
+print(busy_periods)
 
-ggplot2::qplot(data = df, x = hourly_bin_start, y = block_count)
+cumulative_count <- vector()
+for(k in 1:top_block_list_size){
+  cumulative_count[k] = sum(busy_periods$block_count <= busy_periods$block_count[k])
+  y <- as.POSIXlt.POSIXct(busy_periods$hourly_bin_start[k])$year + 1900
+  m <- as.POSIXlt.POSIXct(busy_periods$hourly_bin_start[k])$mon + 1
+  d <- as.POSIXlt.POSIXct(busy_periods$hourly_bin_start[k])$mday
+  bod <- as.POSIXct(
+      # print(
+        paste(y, m, d, sep = "-")
+      # )
+    , tz = "UTC")
+  eod <- bod + 60*60*24
+  print(ggplot2::qplot(data = orig_df[orig_df$hourly_bin_start > bod & orig_df$hourly_bin_start < eod, ], x = hourly_bin_start, y = block_count))
+  
+}
 
+busy_periods <- cbind(busy_periods, cumulative_count)
 
+p <- ggplot(data = busy_periods)
+print(p 
+      + geom_point(aes(x = block_count, y = cumulative_count/top_block_list_size)) 
+      + geom_line(aes(x = block_count, y = cumulative_count/top_block_list_size)) 
+      + xlab("NOTAMs per 4-hour block") 
+      + ylab("Share of busiest blocks having equal or fewer NOTAMs")
+      + labs(title = "Busiest 4-hour blocks as a Cumulative Distribution Function")
+      )
+ggsave("NOTAM_busy_periods_CDF.png", width = 10, height = 5)
