7c81f2c30a8f30b69cd4e49d8540a258df5f5939
lyle-tripp-volpe <59397246+lyle-tripp-volpe@users.noreply.github.com>
2020-01-28 00:37:20 -0500

    cumulative curves added
    this version plots the cumulative arrivals and cumulative departures from the queue.  staffing is simply assumed to be 8 staff throughout the day.

--- NOTAM_cumulative_curves.R
+++ NOTAM_cumulative_curves.R
@@ -3,8 +3,17 @@ library(ggplot2)
 library(grid)
 library(gridExtra)
 library(gtable)
+library(dplyr)
+library(reshape2)
 
 compute_staff_reqd <- function(day) {
+  
+  #begin: staffing paramaters
+  average_NOTAM_processing_time_in_minutes <- 3 #per Bruce W, each processor can process 480 NOTAMs per day
+  nominal_staffing_level <- 10
+  # day = as.Date("2019-08-07", tz = "UTC") #testing
+  #end: staffing parameters
+  
   day_as_datetime <- as.POSIXct.Date(day, origin = "1970-01-01", tz = "UTC")
   minutes_per_day = 24L*60L
   minute <- c(seq(1:minutes_per_day))
@@ -13,40 +22,62 @@ compute_staff_reqd <- function(day) {
   # ordered_arrivals <- arrivals_on_day[order(arrivals_on_day$datetimes),]
   
   minute <- day_as_datetime + (minute-1)*60
-  staffing_df <- data.frame(minute, arrivals, msgs_processed, staff_required)
+  capacity_in_NOTAMs_per_minute_per_staff <- 1 / average_NOTAM_processing_time_in_minutes 
+  s_df <- data.frame(minute, msgs_in_queue = -1, arrivals, msgs_processed, staff_required, staff_available = nominal_staffing_level, capacity = capacity_in_NOTAMs_per_minute_per_staff * nominal_staffing_level)
   
   for(n in 1:minutes_per_day){
-    staffing_df$arrivals[n] = sum(arrivals_on_day$datetimes == staffing_df$minute[n])
-    staffing_df$cml_arrivals[n] = sum(arrivals_on_day$datetimes <= staffing_df$minute[n])
+    s_df$arrivals[n] = sum(arrivals_on_day$datetimes == s_df$minute[n])
+    s_df$cml_arrivals[n] = sum(arrivals_on_day$datetimes <= s_df$minute[n])
+    if (n==1){
+      s_df$msgs_in_queue[n] = max(0, s_df$arrivals[n] - s_df$capacity[n])
+      s_df$msgs_processed[n] = s_df$cml_msgs_processed[n] = min(s_df$arrivals[n], s_df$capacity[n])
+    }
+    # s_df$newly_arriving_msgs_processed[n] = min(s_df$arrivals[n], s_df$capacity[n])
   }
   
-  staffing_df$staff_required = ceiling(staffing_df$arrivals / average_NOTAM_processing_time_in_minutes)
+  # s_df$newly_arriving_msgs_left_in_queue_unprocessed = s_df$arrivals - s_df$new_msgs_processed 
+  # 
+  for(n in 2:minutes_per_day){
+    s_df$msgs_processed[n] = min(s_df$capacity[n], s_df$arrivals[n] + s_df$msgs_in_queue[n-1])
+    s_df$cml_msgs_processed[n] = s_df$cml_msgs_processed[n-1] + s_df$msgs_processed[n]  
+    s_df$msgs_in_queue[n] = s_df$cml_arrivals[n] - s_df$cml_msgs_processed[n] 
+  }
   
-  #begin: plot cumulative curve with estimated instantaneous staffing level
-  p <- ggplot(data = staffing_df) + xlab("minute") 
+  s_df$staff_required = ceiling(s_df$arrivals / average_NOTAM_processing_time_in_minutes)
+  
+  s_df_long = melt(s_df, id = "minute", measure = c("cml_arrivals", "cml_msgs_processed"))
+  
+  #begin: plot cumulative curves with estimated instantaneous staffing level
+  p <- ggplot(data = s_df_long) + xlab("minute") 
   
   pc <- p + 
-    geom_line(aes(x = minute, y = cml_arrivals)) + 
+    aes(minute, value, colour = variable) +
+    aes(minute, value, linetype = variable) +    
+    geom_step() + 
+    guides(colour = "legend", linetype = FALSE) +
+    scale_color_discrete(name = NULL, labels = c("NOTAMs Arrived for Processing", "NOTAMs Processed")) +
+    scale_linetype_discrete() + # scale_linetype_discrete(name = NULL, labels = c(NULL, NULL)) +
+    theme(legend.position = "top") +
     ylab("Cumulative NOTAMs")
+    
+  print(pc)
   
-  ps <- p + 
-    geom_point(aes(x = minute, y = staff_required)) + 
-    ylab("instantaneous staffing requirement")
+  # ps <- p + 
+  #   geom_point(aes(x = minute, y = staff_required)) + 
+  #   ylab("instantaneous staffing requirement")
   
-  g1 <- ggplotGrob(pc)
-  g2 <- ggplotGrob(ps)
-  g <- rbind(g1, g2, size = "first")
-  g$widths <- unit.pmax(g1$widths, g2$widths)
-  grid.newpage()
-  grid.draw(g)
-  #end: plot cumulative curve with estimated instantaneous staffing level
+  # g1 <- ggplotGrob(pc)
+  # g2 <- ggplotGrob(ps)
+  # g <- rbind(g1, g2, size = "first")
+  # g$widths <- unit.pmax(g1$widths, g2$widths)
+  # grid.newpage()
+  # grid.draw(g)
+  #end: plot cumulative curves with estimated instantaneous staffing level
 }
 
-#begin: staffing paramaters
-average_NOTAM_processing_time_in_minutes <- 3 #per Bruce W, each processor can process 480 NOTAMs per day
-# day = as.Date("2019-08-07", tz = "UTC") #testing
-#end: staffing parameters
-for(k in 1:top_block_list_size){
+number_of_busy_days_to_analyze <- 5 #top_block_list_size
+
+for(k in number_of_busy_days_to_analyze:1){
   busy_periods$cumulative_count[k] = sum(busy_periods$block_count <= busy_periods$block_count[k])
   y <- as.POSIXlt.POSIXct(busy_periods$hourly_bin_start[k])$year + 1900
   m <- as.POSIXlt.POSIXct(busy_periods$hourly_bin_start[k])$mon + 1
