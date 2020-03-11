2e719fdb0dc564b593474b9bf0718afbabf34b9c
dflynn-volpe <daniel.flynn@dot.gov>
2020-03-11 12:41:13 -0400

    Additional plotting function for report
    Also some more tables and summaries for report

--- NOTAM_cumulative_curves.R
+++ NOTAM_cumulative_curves.R
@@ -138,6 +138,188 @@ compute_staff_reqd <- function(day, processing_time_in_minutes = 3, hourly_staff
   # end: plot cumulative curves
   }
 
+# Alternative plotting function, with two panels 
+compute_staff_reqd_2 <- function(day, 
+                                 model_name,
+                                 processing_time_in_minutes = 3, 
+                                 hourly_staff_model, 
+                                 study_period_midpoint_utc = as.POSIXct("2019-12-14 23:40:12", tz = "UTC"), study_length_in_minutes = 7*24*60,
+                                 focus_inset = TRUE,
+                                 focus_start = "11:00:00") {
+  # For testing: processing_time_in_minutes = 3; study_period_midpoint_utc = as.POSIXct("2019-12-14 23:40:12", tz = "UTC"); study_length_in_minutes = 7*24*60; focus_inset = TRUE;  focus_start = "11:00:00"
+  
+  # rounding to a whole minute
+  y <- as.POSIXlt.POSIXct(study_period_midpoint_utc)$year + 1900
+  m <- as.POSIXlt.POSIXct(study_period_midpoint_utc)$mon + 1
+  d <- as.POSIXlt.POSIXct(study_period_midpoint_utc)$mday
+  h <- as.POSIXlt.POSIXct(study_period_midpoint_utc)$hour
+  mi <- as.POSIXlt.POSIXct(study_period_midpoint_utc)$min
+  
+  # Create beginning-of-period and end-of-period values for this period
+  mp_string = paste( paste(y, m, d, sep = "-"), paste(h, mi, "00", sep = ":"), sep = " ")
+  mp <- as.POSIXct(mp_string
+                   , tz = "UTC")
+  
+  bod <- as.POSIXct(day, tz = "utc")
+  eod <- bod + 60*60*24
+  
+  bop <- mp - round(0.5*study_length_in_minutes*60)
+  eop <- mp + round(0.5*study_length_in_minutes*60)
+  
+  # begin: staffing paramaters. These are now arguments in the function
+  average_NOTAM_processing_time_in_minutes <- processing_time_in_minutes # Defaults to 3 min, per Bruce W, each processor can process 480 NOTAMs per day
+  # end: staffing parameters
+  
+  day_as_datetime <- as.POSIXct.Date(day, origin = "1970-01-01", tz = "UTC")
+  minutes_per_day = 24L*60L
+  minute <- c(seq(1:minutes_per_day))
+  arrivals <- msgs_departed_from_queue <- staff_required <- vector(length = minutes_per_day)
+  arrivals_on_day <- UniqueInteractions[UniqueInteractions$datetimes >= bod & UniqueInteractions$datetimes < eod,]
+  print(paste("Total NOTAMS in Period: ",count(arrivals_on_day) ))
+  minute <- day_as_datetime + (minute-1)*60
+  capacity_in_NOTAMs_per_minute_per_staff <- 1 / average_NOTAM_processing_time_in_minutes 
+  minutes_per_hour = 60L
+  seconds_per_minute = 60L
+  minute_by_minute_staff_model = rep(hourly_staff_model, each = minutes_per_hour)
+  
+  s_df <- data.frame(minute, msgs_in_queue = NaN, arrivals, msgs_departed_from_queue, staff_required = NaN, staff_available = minute_by_minute_staff_model, capacity = -1)
+  
+  s_df$capacity = s_df$staff_available * capacity_in_NOTAMs_per_minute_per_staff
+  
+  for(n in 1:minutes_per_day){
+    s_df$arrivals[n] = sum(arrivals_on_day$datetimes == s_df$minute[n])
+    s_df$cml_arrivals[n] = sum(arrivals_on_day$datetimes <= s_df$minute[n])
+    if (n==1){
+      s_df$msgs_in_queue[n] = max(0, s_df$arrivals[n] - s_df$capacity[n])
+      s_df$msgs_departed_from_queue[n] = s_df$cml_msgs_departed_from_queue[n] = min(s_df$arrivals[n], s_df$capacity[n])
+    }
+  }
+  
+  for(n in 2:minutes_per_day){
+    s_df$msgs_departed_from_queue[n] = min(s_df$capacity[n], s_df$arrivals[n] + s_df$msgs_in_queue[n-1])
+    s_df$cml_msgs_departed_from_queue[n] = s_df$cml_msgs_departed_from_queue[n-1] + s_df$msgs_departed_from_queue[n]  
+    s_df$msgs_in_queue[n] = s_df$cml_arrivals[n] - s_df$cml_msgs_departed_from_queue[n] 
+  }
+  
+  s_df$cml_staff_minutes_processing_NOTAMS = s_df$cml_msgs_departed_from_queue * average_NOTAM_processing_time_in_minutes
+  total_staff_minutes = sum(s_df$staff_available)
+  
+  # Adding staff time available for non-NOTAM tasks per minute.
+  s_df <- s_df %>%
+    mutate(staff_minutes_available = (capacity - msgs_departed_from_queue )* average_NOTAM_processing_time_in_minutes,
+           cml_staff_hours_available = cumsum(staff_minutes_available) / 60)
+  
+  # begin: build up time-in-system distribution
+  message_count = data.frame(message_count = seq(1:max(s_df$cml_arrivals)))
+  s_df_arrivals = s_df %>% select(cml_arrivals, minute) %>% group_by(cml_arrivals) %>% mutate(earliest = rank(minute) == 1) %>% filter(earliest == TRUE)
+  s_df_msgs_departed_from_queue = s_df %>% mutate(cml_msgs_departed_from_queue = ceiling(cml_msgs_departed_from_queue)) %>% select(cml_msgs_departed_from_queue, minute) %>% group_by(cml_msgs_departed_from_queue) %>% mutate(earliest = rank(minute) == 1) %>% filter(earliest == TRUE)
+  s_df_new = message_count %>% 
+    left_join(s_df_arrivals, by = c("message_count" = "cml_arrivals")) %>% 
+    left_join(s_df_msgs_departed_from_queue, by = c("message_count" = "cml_msgs_departed_from_queue"), suffix = c(".arrivals", ".msgs_departed_from_queue")) %>%
+    fill(minute.arrivals, minute.msgs_departed_from_queue, .direction = "up")
+  s_df_new$minute.msgs_processed = s_df_new$minute.msgs_departed_from_queue + average_NOTAM_processing_time_in_minutes*seconds_per_minute
+  s_df_new$delay = s_df_new$minute.msgs_departed_from_queue - s_df_new$minute.arrivals
+  s_df_new$time_in_system = s_df_new$minute.msgs_processed - s_df_new$minute.arrivals
+  print(s_df_new[which.max(s_df_new$time_in_system),])
+  
+  average_minutes_in_system = round(sum(s_df_new$time_in_system) / max(s_df_new$message_count))
+  average_minutes_delay = round(sum(s_df_new$delay/seconds_per_minute) / max(s_df_new$message_count)) 
+  max_minutes_in_system = max(s_df_new$time_in_system)
+  max_minutes_delay = max(s_df_new$delay/seconds_per_minute)
+  staff_minutes_doing_non_NOTAM_tasks = total_staff_minutes - max(s_df$cml_staff_minutes_processing_NOTAMS)
+  # end: build up time-in-system distribution
+  
+  s_df_long = melt(s_df, id = "minute", measure = c("cml_arrivals", "cml_msgs_departed_from_queue", "staff_available", "staff_minutes_available", "cml_staff_hours_available"))
+  # print(head(s_df_long))
+  # begin: plot cumulative curves
+  # Idea: two panels, one showing just the cumulative curve and the other showing staff count and staff time available for non-NOTAM tasks 
+  p <- ggplot(data = s_df_long) + xlab("Time (UTC)")
+
+  pc <- p + 
+    geom_step(data = s_df_long %>% filter(variable %in% c("cml_arrivals", "cml_msgs_departed_from_queue")),
+              aes(minute, value, colour = variable),
+                  size = 1.25) +
+    
+    guides(colour = "legend", linetype = FALSE) +
+    scale_color_discrete(name = NULL, labels = c("NOTAMs Arrived for Processing", "NOTAMs Departing Queue")) +
+    scale_linetype_discrete() +
+    theme(legend.position = "top",
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 10),
+          legend.text = element_text(size = 10),
+          plot.caption = element_text(hjust = 0.5)) +
+    ylab("Cumulative NOTAMs") +
+    labs(
+      caption = (
+        paste(              
+          "Avg. delay-minutes: ", average_minutes_delay,               
+          "; Max delay-minutes: ", max_minutes_delay,
+          "\n Staff hours doing non-NOTAM tasks: ", round(staff_minutes_doing_non_NOTAM_tasks / 60, 1),
+          "; Staff total hours: ", round(total_staff_minutes / 60, 1)
+        )
+      )
+    )
+  
+  pc_top <- p + 
+    geom_line(data = s_df_long %>% filter(variable == "staff_available") , 
+              aes(minute, value),
+              size = 1.25, colour = 'darkblue') +
+    theme(legend.position = "none", # Remove the legend
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 10),
+          legend.text = element_text(size = 10)) +   
+  ylab("Staffing Level \n")
+  
+  pc_top_2 <- p + 
+    geom_line(data = s_df_long %>% filter(variable == "cml_staff_hours_available") , 
+              aes(minute, value),
+              size = 1.25, colour = 'darkgreen') +
+    theme(legend.position = "none", # Remove the legend
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 10)) +   
+    ylab("Cumulative hours avail. \n for non-NOTAM tasks")
+  
+  # Put these together. egg::ggarrange is better than grid.arrange, because it preserves common axes correctly.
+
+  ga <- egg::ggarrange(pc_top,
+                       pc_top_2,
+                       pc,
+                       ncol = 1,
+                       heights = c(1, 1, 2.5))
+  
+  
+  ggsave(filename = paste0(model_name, '_', day, '.jpeg'),
+         plot = ga,
+         width = 6, height = 7)
+  
+  # Focus plot
+  if(focus_inset){
+    
+    start_min = s_df %>% select(minute) %>% filter(grepl(focus_start, as.character(s_df$minute)))
+    end_min = start_min + 60*60*1 # 1 hour
+    
+    focus_pc <- pc +
+      xlim(unclass(start_min)$minute, unclass(end_min)$minute) +
+      ylim(500, 650)
+    
+    ggsave(filename = paste0('Focus_', model_name, '_', day, '.jpeg'),
+           plot = focus_pc,
+           width = 5, height = 4)
+    
+     
+  }
+  
+  # end: plot cumulative curves
+}
+
+
+
 # Configure busy day analysis ----
 # demand_days_of_interest = as.Date("2017-12-13")
 # demand_days_of_interest = as.Date(c("2019-02-20", "2019-01-31", "2019-01-17", "2018-12-06")) #, "2018-12-09")) # year 2
@@ -335,3 +517,65 @@ staff_df <- as.data.frame(hourly_staff_models_df$staff_list)
 names(staff_df) = hourly_staff_models_df$name
 
 write.csv(staff_df, file = 'NOTAM_Staff_Models_by_hour.csv')
+
+# Identify the specific days and counts of NOTAMs, compared to daily averages for that same month
+
+write.csv(distinct_days %>% select(bod, block_count, percentile),
+          file = 'Distinct_Day_Table.csv',
+          row.names = F)
+
+# Comparison of daily counts in first year of data vs second year
+df <- UniqueInteractions %>% 
+  mutate(weekend = as.factor(ifelse(dow == 'Saturday' | dow == 'Sunday', 1, 0)),
+         quarter = as.factor(quarters(datetimes)),
+         month   = format(date, '%m'),
+         season  = as.factor(ifelse(month == '12' | month == '01' | month == '02', 'Winter',
+                                    ifelse(month == '03' | month == '04' | month == '05', 'Spring',
+                                           ifelse(month == '06' | month == '07' | month == '08', 'Summer',
+                                                  ifelse(month == '09' | month == '10' | month == '11', 'Fall', NA)))))) %>% 
+  group_by(date, month, season, weekend) %>% 
+  summarize(count = n())
+
+df <- df %>%
+  mutate(data_year = ifelse(date < '2018-10-01', 1, 2)) 
+
+df %>%
+  group_by(data_year) %>%
+  summarize(total = sum(count),
+            ave = mean(count),
+            max = max(count))
+
+## Re-do 75th percentile day figures ---- 
+# with some better formatting
+d = 3 # 3rd of distinct_days is the 75th percentile day
+
+day = as.Date(distinct_days$bod[d])
+
+bod <- distinct_days$bod[d]
+eod <- distinct_days$eod[d]  
+  
+# For these figures, only need rows 2 and 6 from hourly_staff_models_df  
+
+  for(x in c(2, 6)){#1:nrow(hourly_staff_models_df)){
+    # x = 2
+    
+    print(model_name <- as.character(hourly_staff_models_df$name[x]))
+    
+    if(hourly_staff_models_df$model_type[x] == "local naive"){
+      hourly_staff_model = as.vector(hourly_staff_models_df$staff_list[x][[1]][[1]])
+    } else {
+      hourly_staff_model = as.vector(orig_df[orig_df$hourly_bin_start >= bod & orig_df$hourly_bin_start < eod, as.character(hourly_staff_models_df$name[x])])
+    }
+    
+    print(hourly_staff_model)
+    
+    # Plot. Function will save .jpeg
+    compute_staff_reqd_2(day = day, processing_time_in_minutes = 3, 
+                         hourly_staff_model = hourly_staff_model, 
+                         model_name = model_name,
+                         focus_inset = TRUE,
+                         focus_start = "11:30:00"
+                         )
+    
+  }
+
