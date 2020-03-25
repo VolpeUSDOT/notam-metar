f0c409aa4c01a2b0b56fb98ca196363dfc5fe787
dflynn-volpe <daniel.flynn@dot.gov>
2020-03-25 14:52:23 -0400

    100th percentile demand day / 90th design day plots
    - Added queue_scale argument to allow better control of queue line plot
    - Addressed NA issue to report time for messages which are processed
    - Fixed label issue to get red for arriving NOTAMs on top

--- NOTAM_Freq/Optional_Plots.R
+++ NOTAM_Freq/Optional_Plots.R
@@ -2,7 +2,16 @@
 # 1. Put red over blue
 # 2. Take processed away and make line for active queue available 
 
+library(tidyverse)
+library(reshape2)
 # Setup ----
+
+# Working directory: This sets the path to my H: drive if being run from my computer, otherwise put in your own path
+workingdir <- ifelse(grepl('flynn', system('whoami', intern = T)),
+                     "H:/Consult/NOTAM + METAR/NOTAM_Freq",
+                     "C:/Users/lylet/OneDrive/Documents/NOTAM/input_output")
+
+setwd(workingdir)
 
 # Start from processed data. 
 load('Processed_NOTAM_Curve_Data.RData')
@@ -19,7 +28,8 @@ compute_staff_reqd_3 <- function(day,
                                  focus_range = c(550, 650),
                                  red_top = F,
                                  plot_queue = F,
-                                 ylim = NULL) {
+                                 ylim = NULL,
+                                 queue_scale = 70) {
   # For testing: processing_time_in_minutes = 3; study_length_in_minutes = 7*24*60; focus_inset = TRUE;  focus_start = "11:00:00"; focus_range = c(550, 650); red_top = T; plot_queue = T
   
   bod <- as.POSIXct(day, tz = "UTC")
@@ -84,10 +94,10 @@ compute_staff_reqd_3 <- function(day,
   s_df_new$time_in_system = s_df_new$minute.msgs_processed - s_df_new$minute.arrivals
   print(s_df_new[which.max(s_df_new$time_in_system),])
   
-  average_minutes_in_system = round(sum(s_df_new$time_in_system) / max(s_df_new$message_count))
-  average_minutes_delay = round(sum(s_df_new$delay/seconds_per_minute) / max(s_df_new$message_count)) 
-  max_minutes_in_system = max(s_df_new$time_in_system)
-  max_minutes_delay = max(s_df_new$delay/seconds_per_minute)
+  average_minutes_in_system = round(sum(s_df_new$time_in_system, na.rm=T) / max(s_df_new$message_count))
+  average_minutes_delay = round(sum(s_df_new$delay/seconds_per_minute, na.rm=T) / max(s_df_new$message_count)) 
+  max_minutes_in_system = max(s_df_new$time_in_system, na.rm=T)
+  max_minutes_delay = max(s_df_new$delay/seconds_per_minute, na.rm=T)
   staff_minutes_doing_non_NOTAM_tasks = total_staff_minutes - max(s_df$cml_staff_minutes_processing_NOTAMS)
   # end: build up time-in-system distribution
   
@@ -104,14 +114,16 @@ compute_staff_reqd_3 <- function(day,
   
   if(red_top){
     pc <- p + 
-      geom_step(data = s_df_long %>% filter(variable %in% c("cml_msgs_departed_from_queue", "cml_arrivals")),
+      geom_step(data = s_df_long %>% filter(variable %in% c("cml_msgs_departed_from_queue")),
                 aes(minute, value, colour = variable),
                 size = 1.25) +
-      
+      geom_step(data = s_df_long %>% filter(variable %in% c("cml_arrivals")),
+                aes(minute, value, colour = variable),
+                size = 1.25) +
       guides(colour = "legend", linetype = FALSE) +
       scale_colour_manual(name = NULL, 
-                          labels = c("NOTAMs Departing Queue", "NOTAMs Arrived for Processing"),
-                          values = c("#00BFC4", "#F8766D")) +
+                          labels = c("NOTAMs Arrived for Processing", "NOTAMs Departing Queue"),
+                          values = c("#F8766D", "#00BFC4")) +
       scale_linetype_discrete() +
       theme(legend.position = "top",
             axis.text = element_text(size = 10),
@@ -165,8 +177,8 @@ compute_staff_reqd_3 <- function(day,
   # Optional: also add line for NOTAMs in queue currently to PC
   if(plot_queue){
     pc <- pc + geom_line(data = s_df_long %>% filter(variable %in% c("msgs_in_queue")),
-                         aes(minute, value * 70)) +
-      scale_y_continuous(sec.axis = sec_axis(trans = ~ . / 70,
+                         aes(minute, value * queue_scale)) +
+      scale_y_continuous(sec.axis = sec_axis(trans = ~ . / queue_scale,
                                              name = "NOTAMs in queue"),
                          limits = ylim,
                          ) +
@@ -210,10 +222,15 @@ compute_staff_reqd_3 <- function(day,
                        ncol = 1,
                        heights = c(1, 1, 2.5))
   
-  if(red_top) { model_name <- paste0('Red_', model_name)}
-  if(plot_queue) {model_name <- paste0('Queue_', model_name)}
+  plot_name1 <- ifelse(grepl('surge', model_name), 'Type_2', 'Type_1')
+  plot_name2 <- sub("naive_", "", model_name)
+  plot_name2 <- sub("_with_surge", "", plot_name2)
+  plot_name <- paste(plot_name1, plot_name2, day, sep = '_')
+  
+  # if(red_top) { plot_name <- paste0('Red_', plot_name)}
+  if(plot_queue) {plot_name <- paste0('Queue_', plot_name)}
      
-  ggsave(filename = paste0(model_name, '_', day, '.jpeg'),
+  ggsave(filename = paste0(plot_name, '.jpeg'),
          plot = ga,
          width = 6, height = 7)
   
@@ -227,7 +244,7 @@ compute_staff_reqd_3 <- function(day,
       xlim(unclass(start_min)$minute, unclass(end_min)$minute) +
       ylim(focus_range[1], focus_range[2])
     
-    ggsave(filename = paste0('Focus_', model_name, '_', day, '.jpeg'),
+    ggsave(filename = paste0('Focus_', plot_name, '.jpeg'),
            plot = focus_pc,
            width = 5, height = 4)
     
@@ -240,17 +257,13 @@ compute_staff_reqd_3 <- function(day,
 
 ## Alternative with red over blue ---- 
 # with some better formatting
-# d = 3 # 3rd of distinct_days is the 75th percentile day
- d = 2 # for 90th
+d = 2 # for 90th
 
 day = as.Date(distinct_days$bod[d])
 
-bod <- distinct_days$bod[d]
-eod <- distinct_days$eod[d]  
-
 # For these figures, only need rows 2 and 6 from hourly_staff_models_df for 75th; 3 and 7 for 90th
 
-for(x in  c(3, 7)){ # c(2, 6)){ #
+for(x in  c(3, 7)){
   # x = 3
   
   print(model_name <- as.character(hourly_staff_models_df$name[x]))
@@ -271,11 +284,11 @@ for(x in  c(3, 7)){ # c(2, 6)){ #
                        plot_queue = F,
                        ylim = c(0, 1250),
                        focus_inset = TRUE,
-                       focus_start =  "16:30:00", #"15:30:00",#
-                       focus_range = c(700, 900) # c(500, 650) #
+                       focus_start =  "16:30:00",
+                       focus_range = c(700, 900) 
   )
  
-  # Option 2: plot queue and secondary axis ----
+  # Option 2: plot queue and secondary axis
   compute_staff_reqd_3(day = day, processing_time_in_minutes = 3, 
                        hourly_staff_model = hourly_staff_model, 
                        model_name = model_name,
@@ -283,14 +296,105 @@ for(x in  c(3, 7)){ # c(2, 6)){ #
                        plot_queue = T,
                        ylim = c(0, 1250),
                        focus_inset = TRUE,
-                       focus_start =  "16:30:00", #"15:30:00",#
-                       focus_range = c(700, 900) # c(500, 650) #
+                       focus_start =  "16:30:00", 
+                       focus_range = c(700, 900) 
+  )
+}
+
+# 75th percentile dat plots
+d = 3 # 3rd of distinct_days is the 75th percentile day
+
+day = as.Date(distinct_days$bod[d])
+
+# For these figures, only need rows 2 and 6 from hourly_staff_models_df for 75th; 3 and 7 for 90th
+
+for(x in  c(2, 6)){ 
+  # x = 3
+  
+  print(model_name <- as.character(hourly_staff_models_df$name[x]))
+  
+  if(hourly_staff_models_df$model_type[x] == "local naive"){
+    hourly_staff_model = as.vector(hourly_staff_models_df$staff_list[x][[1]][[1]])
+  } else {
+    hourly_staff_model = as.vector(orig_df[orig_df$hourly_bin_start >= bod & orig_df$hourly_bin_start < eod, as.character(hourly_staff_models_df$name[x])])
+  }
+  
+  print(hourly_staff_model)
+  
+  # Plot. Function will save .jpeg
+  compute_staff_reqd_3(day = day, processing_time_in_minutes = 3, 
+                       hourly_staff_model = hourly_staff_model, 
+                       model_name = model_name,
+                       red_top = T,
+                       plot_queue = F,
+                       ylim = c(0, 1250),
+                       focus_inset = TRUE,
+                       focus_start =  "15:30:00",
+                       focus_range = c(500, 650) 
+  )
+  
+  # Option 2: plot queue and secondary axis
+  compute_staff_reqd_3(day = day, processing_time_in_minutes = 3, 
+                       hourly_staff_model = hourly_staff_model, 
+                       model_name = model_name,
+                       red_top = T,
+                       plot_queue = T,
+                       ylim = c(0, 1250),
+                       focus_inset = TRUE,
+                       focus_start = "15:30:00",
+                       focus_range = c(500, 650) 
   )
 }
 
 
-# TODO: figure out a way to add to legend when plotting queue 
+# With 90th design applied to 100th demand ----
+# 2 = 90th day, 2018-03-09; 1 = 100th day, 2017-12-14
 
+demand_day = as.Date(distinct_days$bod[1])
 
+# For these figures, only need rows 2 and 6 from hourly_staff_models_df for 75th; 3 and 7 for 90th
 
+# Figure out the right factor to scale the secondary axis for the queue
+# For 90th design and demand, use 70. As Queue gets bigger, scale factor gets smaller to fit on same plot
+queue_scales = c(4, 25)
 
+xi = 1
+for(x in  c(3, 7)){ # 
+  # x = 3
+  
+  print(model_name <- as.character(hourly_staff_models_df$name[x]))
+  
+  if(hourly_staff_models_df$model_type[x] == "local naive"){
+    hourly_staff_model = as.vector(hourly_staff_models_df$staff_list[x][[1]][[1]])
+  } else {
+    hourly_staff_model = as.vector(orig_df[orig_df$hourly_bin_start >= bod & orig_df$hourly_bin_start < eod, as.character(hourly_staff_models_df$name[x])])
+  }
+  
+  print(hourly_staff_model)
+  
+  # Plot. Function will save .jpeg
+  compute_staff_reqd_3(day = demand_day, 
+                       processing_time_in_minutes = 3, 
+                       hourly_staff_model = hourly_staff_model, 
+                       model_name = model_name,
+                       red_top = T,
+                       plot_queue = F,
+                       ylim = c(0, 3200),
+                       queue_scale = queue_scales[xi],
+                       focus_inset = F
+  )
+  
+  # Option 2: plot queue and secondary axis
+  compute_staff_reqd_3(day = demand_day, 
+                       processing_time_in_minutes = 3, 
+                       hourly_staff_model = hourly_staff_model, 
+                       model_name = model_name,
+                       red_top = T,
+                       plot_queue = T,
+                       ylim = c(0, 3200),
+                       queue_scale = queue_scales[xi],
+                       focus_inset = F
+  )
+
+  xi = xi + 1
+  }
